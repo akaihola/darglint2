d01e983d8f615d7487d3a0a052fc483d76a606f0
real-yfprojects <real-yfprojects@users.noreply.github.com>
2023-05-01 11:33:31 +0200

    Add code comment to `default_config` fixture.
    It explains why the fixture is needed.
    
    * tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,7 @@ import darglint2.config
 
 @pytest.fixture(autouse=True)
 def default_config():
+    # darglint2 automatically reads configuration from config files in the dev
+    # environment. We must override config so that we maintain a controlled
+    # test environment.
     darglint2.config._config = darglint2.config.Configuration.get_default_instance()
